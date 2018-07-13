f06b86ed40a052807dcb3b725622f3e7eb168ca5
furkancanby <13408906+furkancanby@users.noreply.github.com>
2018-07-13 15:32:13 +0300

    Update spydirman.py

--- spydirman.py
+++ spydirman.py
@@ -16,27 +16,22 @@ def find_lines(htmltext_beautified_splitted,keywords):
 	for line in htmltext_beautified_splitted:
 		flag = 0
 		for kw in keywords:
-			
 			if kw in line:
 				foundded_lines_numbers.append(line_number)
 				flag=1			
-
 		if flag==0:
 			pass
-
 		line_number += 1
 	return foundded_lines_numbers
 
 # GET LINK AFTER A PARAMETER LIKE ...src="LINK"... 
 def find_withPreword_link_fromLine(line,needed_Preword):
 	inside = ""
-
 	if line.find(needed_Preword) > 0:
 		if needed_Preword != "":
 			pos = int(line.find(needed_Preword)) + 3
 		else:
 			pos = 0
-
 		line= line[pos:]
 		num_q = 0
 		
@@ -44,24 +39,20 @@ def find_withPreword_link_fromLine(line,needed_Preword):
 			if num_q==2:
 				#num_q=0
 				break
-			
 			if num_q==1:
 				if char != "\"":
 					inside += char
 				else:
 					num_q=2
 					continue
-
 			if char == "\"":
 				num_q+=1
-
 		return inside
 	else:
 		return None
 
 # IF THE WEBSITE IS REACHABLE RETURN TRUE
 def check_website(link):
-
 	request = requests.get(str(link))
 	if request.status_code == 200:
 		return True
@@ -77,7 +68,6 @@ def complete_links_withBaseSite(website):
 	last_word = _com[len(_com)-1]
 
 	if last_word == "/":
-		
 		return complete_links_withBaseSite(website[0:len(website)-1])
 	else:
 		return website
@@ -91,17 +81,13 @@ def image_downloader(WEBSITE):
 	
 	beautified = soup.prettify()
 	linebyline = beautified.split("\n")
-
 	print(  str(len(str(page.text).split("\n")) )+  " lines are read from" + SUCCESS_COLOR + " ORIGINAL " + Fore.WHITE +"source code of " + str(WEBSITE))
 	print(  str(len(linebyline) )+ " lines are reduced codes to" + SUCCESS_COLOR + " BEAUTIFIED " + Fore.WHITE +"source code of " + str(WEBSITE))
-	
 	founded_lines = find_lines(linebyline,images)
 	print(Fore.YELLOW+str(images)+"\n"+Fore.CYAN+str(founded_lines))
 
 	for i in range(len(founded_lines)):
-	
 		line =linebyline[ founded_lines[i] ]
-	
 		for pre in images_pre:
 			urL = find_withPreword_link_fromLine(line,pre)
 			#print(urL)
